d6ff28518c42f01fd608fd792a02cde2c57bd88a
Anna Conaway <annaconaway19@gmail.com>
2018-07-22 16:54:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay (musicians, instruments) {
   var array = [];
   for (var i = 0; i < 5; i++) {
-    return (`${musicians[i]} plays ${instruments[i]}`);
+    array(`${musicians[i]} plays ${instruments[i]}`);
   }
   return array;
 }
