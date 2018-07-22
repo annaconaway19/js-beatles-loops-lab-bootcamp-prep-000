40ed34ba584bffa73c116cff1256dcdd267d12e1
Anna Conaway <annaconaway19@gmail.com>
2018-07-22 16:40:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay (musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  while (johnLennonFacts() < facts.length);
+  while (i < facts.length);
   return facts.push("!!!");
 }
 
