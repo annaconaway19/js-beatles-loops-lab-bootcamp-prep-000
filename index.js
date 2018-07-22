6696966aace5e5dfaa46531f1266d43fdba3afe6
Anna Conaway <annaconaway19@gmail.com>
2018-07-22 16:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < 5; i++) {
+    return (`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  while (facts)
+  return facts.push("!!!");
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    [array.push("I love the Beatles!")]
+  } while (number < 15);
+}
